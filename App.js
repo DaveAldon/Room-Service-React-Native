39b2cd0f4eb724a8737f2de0a9220d5d01079862
David Crawford <dcrawford@spotinc.com>
2021-01-21 13:45:52 -0500

    Removed unused code.

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from "react-native";
-
-import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from "react-native/Libraries/NewAppScreen";
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { RoomServiceProvider } from "@roomservice/react";
-import { RoomServiceHome } from "./components/index";
+import { RoomServiceHome } from "./components/RoomServiceHome/index";
 
 async function AuthCheck(params) {
   const response = await fetch("http://172.16.29.51:3000/api/roomservice", {
@@ -23,7 +21,6 @@ async function AuthCheck(params) {
   }
 
   const body = await response.json();
-  console.log("body...", body);
   return body;
 }
 
@@ -49,43 +46,6 @@ function CreateGuid() {
   return _p8() + _p8(true) + _p8(true) + _p8();
 }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: "absolute",
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right",
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
